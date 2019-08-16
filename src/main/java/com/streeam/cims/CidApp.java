d21aa10b7cdd384d38fcb7accf0dc08e860e182a
Streeam <bogdanmihoci_86@yahoo.com>
2019-08-16 11:24:48 +0100

    Updated the API documentation

--- src/main/java/com/streeam/cims/CidApp.java
+++ src/main/java/com/streeam/cims/CidApp.java
@@ -37,31 +37,54 @@ public class CidApp implements InitializingBean {
         //TODO 1. api/companies
             //TODO POST Create a company and automatically become the manager                                                                        *****DONE********
                     // * If the user is ROLE_MANAGER or ROLE_EMPLOYEE he cannot see this option otherwise he can
-            //TODO GET List all the companies details (manager can see only his company, admin can see all companies)                               *********DONE********
-
+                    // Only the ROLE_USER can create a company
+                    // He then automatically becomes MANAGER
+            //TODO GET List all the companies details                                                                                              *********DONE********
+                      // manager and employee can see only his company, admin and user can see all companies
             //TODO DELETE Delete a company  (manager can delete only his company, admin can delete any companies)                                   **********DONE********
                     // Get all the employees from the company and remove the employee role. Remove the managers roles
-
+                    // Delete the company update the employee company id
             //TODO PUT Update a company (manager his , admin any)
-
+                    // Only if you are a manager or admin you can update the company (except the company's email)
         //TODO 2. api/activate                                                                                                                      ********DONE********
                 // GET When activating the user also create and links to an employee (NEEDS TEST)
         //TODO 3. api/employee/employeeId/request-to-join (no restriction)
             //TODO POST(companyName) Request to join a company
                 // * If the user is ROLE_MANAGER and ROLE_EMPLOYEE he cannot see this option
-                // * If the user is not ROLE_MANAGER nor ROLE_EMPLOYEE he can send a request to join the company
-        //TODO 4. api/invite-to-join (Pre-Authorize ROLE_MANAGER)
+                // * Sends a email to the company's manager to request to join the company
+                // * Creates a notification and sends it to the manager
+        //TODO 4. api/invite-to-join (Pre-Authorize ROLE_MANAGER or ROLE_ADMIN)
             //TODO POST(email)
                 // * If the user exists and is not ROLE_MANAGER and ROLE_EMPLOYEE sends a notification and an email to the user
                 // * If the user don't exists send an email with link to registration page
-        //TODO 5. api/employees/employeeId/approve-employee
-
-        //TODO 6. api/employees/employeeId/fire-employee
-
-        //TODO 7. api/employees/employeeId/leave-company
-
-        //TODO 8. api/users
+        //TODO 5. api/employees/{employeeId}/approve-employee (Pre-Authorize ROLE_MANAGER or ROLE_ADMIN)
+                // * The manager or the admin approves the users request.
+                // * The user gets the ROLE_EMPLOYEE and it is added to the company (save the user, employee and the company)
+                // * Sends a email to the user to inform him that his request has been approved
+                // * Also sends a notification to the user to inform him that his request has been approved.
+        //TODO 5. api/employees/{employeeId}/reject-employee
+                    // * The manager or the admin rejects the users request.
+                    // * Sends a email to the user to inform him that his request has been rejected
+                    // * Also sends a notification to the user to inform him that his request has been rejected.
+        //TODO 6. api/employees/{employeeId}/fire-employee (Pre-Authorize ROLE_MANAGER or ROLE_ADMIN)
+                    // The manager or the admin can fire an employee.
+                    // Removes all the users roles except the default ROLE_USER (update the user, the employee and the company)
+                    // Send a notification to the user informing him that he got fired
+        //TODO 7. api/employees/{employeeId}/leave-company (Pre-Authorize ROLE_EMPLOYEE)
+                    // The employee resigns from the company
+                    // Removes all the users roles except the default ROLE_USER (update the user, the employee and the company)
+                    // Send a notification to all the employees from the company to inform that he is leaving the company
+        //TODO 8. api/users (Pre-Authorize ROLE_ADMIN)
                 // POST When admin creates a user also creates an employee
+                // DELETE when admin deletes an employee it also deletes the linked user and updates the company if he is in one
+                // PUT  When employee is updated the user is updated as well
+
+        //TODO 8. api/employees
+                // GET (ADMIN can see all, the rest can only see their own account)
+                // POST  (Pre-Authorize ROLE_ADMIN) when admin creates an employee it also creates user with the same credentials
+                // DELETE  (Pre-Authorize ROLE_ADMIN) when admin deletes an employee it also deletes the linked user and updates the company if he is in one. Also delete all notification related to this employee
+                // PUT  (ADMIN can update all, Manager can all from his company,  the rest can only update their own account)
+                        // When employee is updated the user is updated as well (no one can cannot updated the email). The admin and managers can also update the employee roles
 
 
 
